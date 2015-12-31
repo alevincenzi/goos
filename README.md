f715cf31d6469fe1a99ea1f053d42dfacc3b4cb5
Ale Vincenzi <alessandro84.vincenzi@gmail.com>
2015-12-31 17:08:14 +0100

    - Created an Eclipse project - Written first EndToEend test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goos
 Growing Object Oriented Software guided by Tests
+
+- Java 8
+- Eclipse 4.5.1
---
+++ test/endtoend/AuctionSniperEndToEndTest.java
@@ -0,0 +1,36 @@
+package test.endtoend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class AuctionSniperEndToEndTest {
+
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+	
+	@Test
+	public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+		
+		auction.startSellingItem();
+		
+		application.startBiddingIn(auction);
+		
+		auction.hasReceivedJoinRequestFromSniper();
+		auction.announceClosed();
+		
+		application.showsSniperHasLostAuction();
+	}
+	
+	@After
+	public void stopAcution() {
+		auction.stop();
+	}
+	
+	@After
+	public void stopApplication() {
+		application.stop();
+	}
+
+}
