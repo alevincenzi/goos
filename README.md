2338d455280032ddc66f34394b30ce84ebec854e
Ale Vincenzi <alessandro84.vincenzi@gmail.com>
2016-01-01 10:39:57 +0100

    - Added class ApplicationRunner

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# goos
+# Goos
 Growing Object Oriented Software guided by Tests
 
 - Java 8
 - Eclipse 4.5.1
+- Openfire 3.10.2. 
+
+Openfire
+
+user/pwd
+  - sniper/sniper
+  - auction-item-54321/auction
+  - auction item-65432/auction
+
+Server Settings / Resource Policy / Never Kick
+Server Settings / Offline Messages / Drop
+
---
+++ test/endtoend/ApplicationRunner.java
@@ -0,0 +1,43 @@
+package endtoend;
+
+import sun.applet.Main;
+
+public class ApplicationRunner {
+	
+	public static final String SNIPER_ID = "sniper";
+	public static final String SNIPER_PASSWORD = "sniper";
+	
+	private AuctionSniperDriver driver;
+
+	public void startBiddingIn(final FakeAuctionServer auction){
+		
+		Thread thread = new Thread("Test application") {
+			
+			@Override
+			public void run(){
+				
+				try {
+					Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		thread.setDaemon(true);
+		thread.start();
+		
+		driver = new AuctionSniperDriver(1000);
+		driver.showSniperStatus(Main.STATUS_JOINING);
+	}
+	
+	public void showsSniperHasLostAuction(){
+		driver.showSniperStatus(Main.STATUS_LOST);
+	}
+	
+	public void stop(){
+		if (driver != null)
+			driver.dispose();
+	}
+}
