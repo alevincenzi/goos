93857f15c22bd726100ede097c7c6a46ce27c45e
Ale Vincenzi <alessandro84.vincenzi@gmail.com>
2016-01-01 11:03:36 +0100

    - Added local libraries for WindowLicker - Added class AuctionSniperDriver

---
+++ test/endtoend/AuctionSniperDriver.java
@@ -0,0 +1,33 @@
+package endtoend;
+
+import javax.swing.JFrame;
+
+import org.hamcrest.Matcher;
+
+import com.objogate.wl.Prober;
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+public class AuctionSniperDriver extends JFrameDriver {
+
+	public AuctionSniperDriver(int timeoutMilliSeconds) {
+		
+		super(
+				new GesturePerformer(),
+
+				JFrameDriver.topLevelFrame(
+						named(MainWindow.MAIN_WINDOW_NAME),
+						showingOnScreen()),
+				
+				new AWTEventQueueProber(timeoutMilliSeconds, 100)
+		);
+	}
+	
+	public void showSniperStatus(String statusText){
+		new JLabelDriver(
+				this, named(MAIN.SNIPER_STATUS_NAME).hasText(equalTo(statusText)));
+	}
+
+}
