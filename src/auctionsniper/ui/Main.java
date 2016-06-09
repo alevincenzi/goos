b8b97087c09f260d0eb65f592df1326d346903ed
Ale Vincenzi <alessandro84.vincenzi@gmail.com>
2016-06-09 20:09:33 +0200

    - Sending join request to auction - sniperJoinsAuctionUntilAuctionCloses Fails (cannot find text Lost)

--- src/auctionsniper/ui/Main.java
+++ src/auctionsniper/ui/Main.java
@@ -7,6 +7,12 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 public class Main {
 
 	public static final String MAIN_WINDOW_NAME   = "Auction Sniper Main";
@@ -14,6 +20,10 @@ public class Main {
 	
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST    = "Lost";
+	
+	private static final String AUCTION_RESOURCE  = "Auction";
+	private static final String ITEM_ID_AS_LOGIN  = "auction-%s";
+	private static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 	
 	private MainWindow ui;
 	
@@ -60,5 +70,33 @@ public class Main {
 			String itemId) throws Exception {
 		
 		Main main = new Main();
+		
+		XMPPConnection connection = connectTo(xmppHostname, sniperId, sniperPassword);
+		
+		Chat chat = connection.getChatManager().createChat(
+		
+				auctionId(itemId, connection),
+				new MessageListener() {
+					@Override
+					public void processMessage(Chat chat, Message message) {
+						// nothing yet
+					}
+				});
+
+		chat.sendMessage(new Message());
+	}
+	
+	private static XMPPConnection
+	connectTo(String hostname, String username, String password) throws XMPPException {
+	
+		XMPPConnection connection = new XMPPConnection(hostname);
+		connection.connect();
+		connection.login(username, password, AUCTION_RESOURCE);
+		return connection;
+	}
+	
+	private static String auctionId(String itemId, XMPPConnection connection) {
+
+		return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
 	}
 }
