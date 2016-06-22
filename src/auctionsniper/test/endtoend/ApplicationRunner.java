8ca3d0427d33cb0a7d4c9b5e2649c72cd3122e0c
Ale Vincenzi <alessandro84.vincenzi@gmail.com>
2016-06-22 19:48:43 +0200

    Added new EtE test sniperWinsAnAuctionByBiddingHigher

--- src/auctionsniper/test/endtoend/ApplicationRunner.java
+++ src/auctionsniper/test/endtoend/ApplicationRunner.java
@@ -44,6 +44,15 @@ public class ApplicationRunner {
 		driver.showsSniperStatus(MainWindow.STATUS_BIDDING);
 	}
 
+	public void hasShownSniperIsWinning() {
+		driver.showsSniperStatus(MainWindow.STATUS_WINNING);
+	}
+	
+	public void showsSniperHasWonAuction() {
+		driver.showsSniperStatus(MainWindow.STATUS_WON);
+	}
+	
+	
 	public void stop(){
 		if (driver != null)
 			driver.dispose();
--- src/auctionsniper/ui/Main.java
+++ src/auctionsniper/ui/Main.java
@@ -49,6 +49,8 @@ public class Main {
 		public static final String STATUS_JOINING = "Joining";
 		public static final String STATUS_LOST    = "Lost";
 		public static final String STATUS_BIDDING = "Bidding";
+		public static final String STATUS_WINNING = "Winning";
+		public static final String STATUS_WON     = "Won";
 		
 		private final JLabel sniperStatus = createLabel(STATUS_JOINING);
 
